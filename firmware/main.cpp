e070e60c8d4eb3e411df087d6cb0f3f4651802f0
Andrey Gusakov <dron0gus@gmail.com>
2021-08-01 11:31:35 +0300

    DEBUG: use MPU to protect flash bank2 and option bytes
    ...try to catch access

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -40,6 +40,46 @@ systime_t last_print_tm = 0;
 
 #endif
 
+void mpu_debug(BaseSequentialStream *chp)
+{
+	int i;
+
+	/* protect second banck of flash */
+	ARM_MPU_SetRegionEx(6,
+		0x08100000,
+		(1 << 28) | /* no instruction fetching */
+		(0 << 24) | /* no access */
+		(0 << 19) | /* TEX */
+		(0 << 17) | /* C */
+		(0 << 16) | /* B */
+		(0 << 8)  | /* enable all sub-regs */
+		(ARM_MPU_REGION_SIZE_1MB << 1) | /* size */
+		(1 << 0)  | /* Region enable */
+		0);
+	/* protect information bock */
+	ARM_MPU_SetRegionEx(5,
+		0x1FF00000,
+		(1 << 28) | /* no instruction fetching */
+		(0 << 24) | /* no access */
+		(0 << 19) | /* TEX */
+		(0 << 17) | /* C */
+		(0 << 16) | /* B */
+		(0 << 8)  | /* enable all sub-regs */
+		(ARM_MPU_REGION_SIZE_64KB << 1) | /* size */
+		(1 << 0)  | /* Region enable */
+		0);
+
+	for (i = 0; i < 10; i++)
+		chprintf(chp, "Fu\r\n");
+	chprintf(chp, "MPU type: 0x%08x, ctrl 0x%08x\r\n", MPU->TYPE, MPU->CTRL);
+	for (i = 0; i < 8; i++) {
+		MPU->RNR = i;
+
+		chprintf(chp, "MPU%02d: RBAR 0x%08x RASR 0x%08x\r\n",
+			i, MPU->RBAR, MPU->RASR);
+	}
+}
+
 int main(void) {
 	/*
 	 * ChibiOS/RT initialization
@@ -61,6 +101,8 @@ int main(void) {
 
 	chThdCreateStatic(shellStack, sizeof(shellStack), NORMALPRIO + 1, shellThread, (void *)&shell_cfg);
 #endif
+
+	mpu_debug((BaseSequentialStream *)&EFI_SERIAL_SHELL_SD);
 
 	/**
 	 * most basic MCU initialization - no configuration access, no external hardware access
